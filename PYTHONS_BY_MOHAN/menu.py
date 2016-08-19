d05235c3b77ef258cb791fa6c7a3aaa8ec7e5ecb
Mohan Pugaz <mohanpugaz@gmail.com>
2016-08-19 17:47:25 +0530

    Create menu.py

---
+++ PYTHONS_BY_MOHAN/menu.py
@@ -0,0 +1,6 @@
+Mn_Toolbar=nuke.toolbar("Mo Nodes")
+cfToolar=Mn_Toolbar.addMenu('Create_Folder', icon="add_folder.png")
+
+n=nuke.menu('Nuke')
+cfMenu=n.addMenu('Mo_Tools/Create_Folder', icon="add_folder.png")
+
