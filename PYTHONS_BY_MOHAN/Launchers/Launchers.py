a4425419497192a2d91c99e3abfc1a031f710c2e
Mohan Pugaz <mohanpugaz@gmail.com>
2016-08-19 17:30:20 +0530

    Create Launchers.py

---
+++ PYTHONS_BY_MOHAN/Launchers/Launchers.py
@@ -0,0 +1,15 @@
+import os
+
+def openMochaPro():
+    os.system('mochapro&')
+    return
+def openMpcMail():
+    os.system('mpcmail&')
+    return
+    
+def openSil():
+    os.system('silhouette&')
+    return
+def openSyncIt():
+    os.system('syncIt&')
+    return
